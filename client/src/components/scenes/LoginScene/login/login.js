1546b5dfc715e325566da30aa81923f0e54160d1
khai93 <33293519+khai93@users.noreply.github.com>
2020-10-10 19:11:35 -0500

    fix login url

--- client/src/components/scenes/LoginScene/login/login.js
+++ client/src/components/scenes/LoginScene/login/login.js
@@ -7,7 +7,7 @@ import './login.css';
 function Login() {
 
     const clickHandler = () => {
-        window.location = SERVER_URL + "/api/auth/discord";
+        window.location.href = SERVER_URL + "/api/auth/discord";
     }
 
     return (
